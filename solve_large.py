3377996320c785311cf0e3b6272137225ec6e877
Lior Sinai <sinailior@gmail.com>
2020-10-20 19:47:40 +0200

    Added a whole bunch of puzzles. Most of these are very very hard and my solver cannot solve them

--- solve_large.py
+++ solve_large.py
@@ -10,6 +10,9 @@ import time
 import matplotlib.pyplot as plt
 from nonogramSolver import Nonogram, plot_nonogram
 
+BLACK = 1   # = 01 in binary
+WHITE = 2   # = 10 in binary
+EITHER = 3  # = 11 in binary
 
 def encode(runs_row, runs_col, filename="large_puzzle.txt"):
     max_rule = 0
@@ -38,6 +41,7 @@ def encode(runs_row, runs_col, filename="large_puzzle.txt"):
 def decode(filename: str):
     runs_row = []
     runs_col = []
+    solution = None
 
     writing = False
     with open(filename, "r") as file:
@@ -53,31 +57,57 @@ def decode(filename: str):
                 writing = True
             elif line == 'columns':
                 active_runs = runs_col
+            elif line[:4] == 'goal':
+                solution = line[6:-1]
             elif writing:
+                line = line.strip(',')
                 active_runs.append(tuple(map(int, line.split(','))))
 
-    return runs_row, runs_col
+    return runs_row, runs_col, solution
 
+
+def decode_solution(solution, n_rows, n_cols):
+    convertor = {"0": WHITE, "1": BLACK}
+
+    solution = [solution[i * n_cols:i * n_cols +n_cols] for i in range(0, n_rows)]
+
+    grid = []
+    for row in solution:
+        grid.append([convertor[c] for c in row])
+    return grid
 
 if __name__ == '__main__':
     #file_name = "lost_puzzle.txt"
-    file_name = "beach_puzzle.txt" # can't solve
-    #file_name = "artist_puzzle.txt"
+    #file_name = "beach_puzzle.txt" # can't solve
+    #file_name = "artist_puzzle.txt" # faster with match_forwards than match backwards
     #file_name = "balance_puzzle.txt"
     #file_name = "warship_puzzle.txt"
+    file_name = "bear.txt"
 
-    runs_row, runs_col = decode(file_name)
+    # the webpbn puzzles are super hard
+    #file_name = "webpbn-00065-Mum's the Word.txt"
 
+    file_name = 'puzzles/'+ file_name
+    runs_row, runs_col, solution = decode(file_name)
     game = Nonogram(runs_row, runs_col)
 
-    ## solve game
-    start_time = time.time()
-    game.solve_fast()
-    end_time = time.time()
+    solve = True
+    # set solution
+    if solution and not solve: # the webpbn files have solutions
+        print("setting solution ...")
+        grid_sol = decode_solution(solution, len(runs_row), len(runs_col))
+        game.set_grid(grid_sol)
 
-    game.show_grid(show_instructions=False, symbols="x#.?") # these are too big to print on the command line
-    print(game.is_complete(), "{:.2f}%%".format(game.progress*100))
-    print("time taken: {:.5f}s".format(end_time - start_time))
+    ##solve game
+    if solve:
+        start_time = time.time()
+        game.solve_fast(make_guess=False)
+        #game.solve()
+        end_time = time.time()
+        #game.show_grid(show_instructions=False, symbols="x#.?") # these are very big to print on the command line
+        print(game.is_complete(), "{:.2f}%%".format(game.progress*100))
+        print("time taken: {:.5f}s".format(end_time - start_time))
+
     plot_nonogram(game.grid)
 
     plt.show()
